69b70ff37e4aa540990a158663713d492366f4ea
Daniel Ortiz <daniel@suntus.mx>
2025-01-08 16:01:47 -0600

    Enhance error handling in saveProspect function to include DatabaseError and log error details

--- database.helpers.js
+++ database.helpers.js
@@ -1,15 +1,19 @@
 const { ProspectsModel, UserTypesModel } = require('./models.js');
-const { UniqueConstraintError } = require = require('sequelize');
+const { UniqueConstraintError, DatabaseError, ValidationErrorItem } = require = require('sequelize');
 
 
 const saveProspect = async (prospect) => {
     try {
         const insertedProspect = await ProspectsModel.create(prospect);
+        console.log(insertedProspect);
         return insertedProspect;
     } catch (error) {
+        console.log(error);
 
         if (error instanceof UniqueConstraintError) {
-            throw new Error('This email is already in use');
+            throw new Error(error.message);
+        } else if (error instanceof DatabaseError) {
+            throw new Error(error.message);
         }
 
     };
