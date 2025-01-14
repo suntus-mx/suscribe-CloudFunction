38dbd9c63ccc6a09bbdf08040720d684c441f02f
Daniel Ortiz <daniel@suntus.mx>
2025-01-14 06:36:44 -0600

    Add getQuestionsbyUserType function and improve error handling in saveProspect

--- database.helpers.js
+++ database.helpers.js
@@ -1,5 +1,6 @@
-const { ProspectsModel, UserTypesModel } = require('./models.js');
+const { ProspectsModel, UserTypesModel, QuestionsModel } = require('./models.js');
 const { UniqueConstraintError, DatabaseError, ValidationErrorItem } = require = require('sequelize');
+const connection = require('./database.js');
 
 
 const saveProspect = async (prospect) => {
@@ -11,12 +12,29 @@ const saveProspect = async (prospect) => {
         console.log(error);
 
         if (error instanceof UniqueConstraintError) {
-            throw new Error(error.message);
+            throw new Error(error.errors[0].message);
         } else if (error instanceof DatabaseError) {
-            throw new Error(error.message);
+            throw new Error(error.errors[0].message);
         }
 
     };
 };
 
-module.exports = saveProspect;
+const getQuestionsbyUserType = async (userTypeId) => {
+    try {
+        const questions = await QuestionsModel.findAll({
+            where: {
+                userTypeId: userTypeId
+            }
+        });
+        return questions;
+    } catch (error) {
+        console.log(error);
+        throw new Error(error.errors[0].message);
+    }
+};
+
+module.exports = {
+    saveProspect,
+    getQuestionsbyUserType
+};
